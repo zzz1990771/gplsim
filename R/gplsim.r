26cfeeaa96e06a6ddd4eb013bd10f65c50c205aa
zzz1990771 <zzz1990771@gmail.com>
2021-11-24 11:45:24 -0500

    update to 0.9.0

--- R/gplsim.r
+++ R/gplsim.r
@@ -1,5 +1,17 @@
-## Data generation function for simulation and demonstration
-## A sine-bump setting has been employed.
+#' Data generation function for simulation and demonstration
+#' A sine-bump setting has been employed.
+#'
+#' @param n sample size
+#' @param true.theta true single-index coefficients,
+#' default is c(1,1,1)/sqrt(3) for setting 1 and c(1,2)/sqrt(5) for other settings
+#' @param family chose from "guassian", "binomial" or "poisson".
+#' @param ncopy generates multiple copies of data for Monte Carlo simulations
+#'
+#' @return X single index predictors
+#' @return Y response variables, a list
+#' @return Z partial linear predictor(s)
+#' @return single_index_values single index term
+#' @export
 generate_data <- function(n,true.theta=c(1, 1, 1)/sqrt(3),family="gaussian",ncopy=1){
   #parameter setting
   sigma = 0.1
@@ -7,7 +19,7 @@ generate_data <- function(n,true.theta=c(1, 1, 1)/sqrt(3),family="gaussian",ncop
   c2 = 1.3409
   rho = 0.3
 
-  X = matrix(runif(length(true.theta)*n), ncol=length(true.theta))
+  X = matrix(stats::runif(length(true.theta)*n), ncol=length(true.theta))
   U = X%*%true.theta
   fU = sin( (U-c1)*pi/(c2 -c1) )
 
@@ -26,11 +38,25 @@ generate_data <- function(n,true.theta=c(1, 1, 1)/sqrt(3),family="gaussian",ncop
   }
   if(ncopy==1){ylist = ylist[[1]]}
   return(list("X" = X, "Y" = ylist,"Z"=Z,"single_index_values"=fU))
-  
 }
 
-#internal supporting function: single index function
-si <- function(alpha,y,x,z,opt=TRUE,k=13,smooth_selection,fam,bs="tr", fx=FALSE,scale=scale) {
+#' An internal function to optimization and fitting. 
+#' Don't use it solely.
+#'
+#' @param alpha single-index coefficients
+#' @param y Response variable, should be a vector.
+#' @param x Single index covariates.
+#' @param z Partially linear covariates.
+#' @param opt see ?gplsim
+#' @param k see ?gplsim
+#' @param smooth_selection see ?gplsim
+#' @param fam see ?gplsim
+#' @param bs see ?gplsim
+#' @param fx see ?gplsim
+#' @param scale see ?gplsim
+#' 
+#' @return b fitted gam object
+si <- function(alpha,y,x,z,opt=TRUE,k=13,smooth_selection,fam,bs="ps", fx=FALSE,scale=scale) {
   ## Fit single index model using gam call. Return ML is opt==TRUE
   ## and fitted gam with theta added otherwise...
 
@@ -53,11 +79,103 @@ si <- function(alpha,y,x,z,opt=TRUE,k=13,smooth_selection,fam,bs="tr", fx=FALSE,
 
 }
 
+#' Function to fit generalised partially linear single-index models via penalised splines
+#'
+#' This function employs penalised spline (P-spline) to estimate generalised
+#' partially linear single index models, which extend the generalised linear
+#' models to include nonlinear effect for some predictors.
+#'
+#' @param Y Response variable, should be a vector.
+#' @param X Single index covariates.
+#' @param Z Partially linear covariates.
+#' @param family A \code{family} object: a list of functions and expressions
+#' for defining \code{link} and \code{variance} functions.
+#' Families supported are \code{binomial}, \code{gaussian}.
+#' The default family is \code{gaussian}.
+#' @param penalty Whether use penalised splines or un-penalised splines to fit the model. The default is TRUE.
+#' @param penalty_type The optional argument penalty_type is a character variable, which specifies the type of penalty used in the penalized splines estimation. The default penalty type is {L}_{2} penalty, while {L}_{1} is also supported.
+#' @param scale The optional argument scale is a numeric indicator with a default value set to -1. Any negative value including -1 indicates that the scale of response distribution is unknown, thus need to be estimated. Another option is 0 signaling scale of 1 for Poisson and binomial distribution and unknown for others. Any positive value will be taken as the known scale parameter.
+#' @param smooth_selection The optional argument smooth_selection is another character variable that specifies the criterion used in the selection of a smoothing parameter. The supported criteria include "GCV.Cp","GACV.Cp", "ML","P-ML", "P-REML" and "REML", while the default criterion is "GCV.Cp".
+#' @param profile profile is a logical variable that indicates whether the algorithm with profile likelihood or algorithm with NLS procedure should be used. The default algorithm is set to algorithm with profile likelihood.
+#' @param bs bs is a character variable that specifies the spline basis in the estimation of unknown univariate function of single index. Default is P-slines.
+#' @param user.init The user.init is a numeric vector of the same length as the dimensionality of single index predictors. The users can use this argument to pass in any appropriate user-defined initial single-index coefficients based on prior information or domain knowledge. The default value is NULL
+#' @param k k is the the dimension of the basis used to represent the smooth term. The default is set at 13.
+#' 
+#' @return theta  Estimation of Theta
+#' @return coefficients  the coefficients of the fitted model. Parametric coefficients are first, followed by coefficients for each spline term in turn.
+#' @return ... See GAM object
+#' @examples
+#' # parameter settings
+#' n=1000
+#' true.theta = c(1, 1, 1)/sqrt(3)
 
-## main model fitting function
+#' # Generating data (binary case)
+#' # This function generates a sin bump model as in Yu et al. (2007).
+#' # You may use your data instead.
+#' data <- generate_data(n,true.theta=true.theta,family="binomial")
+#' y=data$Y       # binary response
+#' X=data$X       # single index term ;
+#' Z=data$Z       # partially linear term ;
+#'
+#' # Fit the generalised partially linear single-index models
+#' result <- gplsim(y,X,Z,family = binomial)
+#'
+#' # Estimation of Theta
+#' result$theta
+#'
+#' # the coefficients of the fitted model. Parametric coefficients are first, followed by coefficients for each spline term in turn.
+#' result$coefficients
+#'
+#' # summary of the fitted model
+#' summary(result)
+#'
+#' #plot the estimated single index function curve
+#' plot.si(result)
+#' #par(new=T)
+#' #plot.si(result,index=Z,xaxt="n", yaxt="n",col="red")
+#'
+#'
+#' # Gaussian case
+#' # This function generate a plain sin bump model with gaussian response.
+#' data <- generate_data(n,true.theta=true.theta,family="gaussian")
+#' y=data$Y       # continous response
+#' X=data$X       # single index term ;
+#' Z=data$Z       # partially linear term ;
+#'
+#' result <- gplsim(y,X,Z,family = gaussian)
+#' result$theta
+#' result$coefficients
+#' summary(result)
+#'
+#'
+#' #plot the estimated single index function curve
+#' plot.si(result)
+#' #par(new=T)
+#' #plot.si(result,index=Z,xaxt="n", yaxt="n",col="red")
+#'
+#' # A real data example
+#' data(air)
+#' y=air$ozone               # response
+#' X=as.matrix(air[,3:4])    # single index term ;
+#' Z=air[,2]                 # partially linear term ;
+#'
+#' result <- gplsim(y,X,Z=Z,family = gaussian,k=10)
+#' result$theta
+#' result$coefficients
+#' summary(result)
+#'
+#' # Or you can try different spline basis
+#' result <- gplsim(y,X,Z=Z,family = gaussian,bs="tp",k=10)
+#' result$theta
+#' result$coefficients
+#' summary(result)
+#'
+#' # to know more about air data
+#' ?air
+#' @export
+gplsim <- function(Y=Y,X=X,Z=Z,family=gaussian(),penalty=TRUE,penalty_type = "L2", scale = -1, smooth_selection = "GCV.Cp",profile = TRUE, bs="ps", user.init=NULL,k=13){
 
-gplsim<- gplsimPs <- function(Y=Y,X=X,Z=Z,family=gaussian,penalty=TRUE,penalty_type = "L2", scale = -1, smooth_selection = "GCV.Cp",profile = TRUE, bs="ps", user.init=NULL,k=13){
-  #if p <= 2
+#if p <= 2
   p <- dim(X)[2]
   if(p<2){stop("SIM predictos must no less than two")}
 
@@ -105,29 +223,43 @@ gplsim<- gplsimPs <- function(Y=Y,X=X,Z=Z,family=gaussian,penalty=TRUE,penalty_t
   return(b)
 }
 
-#Function that plot fitted curve for the unknown univariate function for single index term
-plot.si <- function(model_obj,family=gaussian(),plot_data=FALSE,ylab = "mean",yscale=NULL,...){
-  if(is.null(model_obj$Znames)){
-    offset_fit <- family$linkinv((model_obj$linear.predictors))
+utils::globalVariables(c("yscale"))
+
+#' Function that plot fitted curve for the unknown univariate function for single index term
+#' 
+#' @param x the gam/gplism fitted object
+#' @param ... add-on arguments
+#'
+#' @return NULL single-index plot
+#' @export plot.si
+#' @export
+plot.si <- function(x,...){
+  #check the args
+  family=gaussian()
+  plot_data= ifelse(!exists("plot_data"),FALSE,plot_data)
+  ylab= ifelse(!exists("ylab"),"mean",ylab)
+  
+
+  if(is.null(x$Znames)){
+    offset_fit <- family$linkinv((x$linear.predictors))
   }else{
-    offset_fit <- family$linkinv((model_obj$linear.predictors-model_obj$gamma%*%t(model_obj$model$z)))
+    offset_fit <- family$linkinv((x$linear.predictors-x$gamma%*%t(x$model$z)))
   }
 
-  UQ = cbind(model_obj$model$a,matrix(offset_fit,ncol=1))
-  if(is.null(yscale)){ylim=range(offset_fit)}else{ylim=yscale}
+  UQ = cbind(x$model$a,matrix(offset_fit,ncol=1))
+  if(!exists("yscale")){ylim = range(offset_fit)}else{ylim = yscale}
   plot(UQ[order(UQ[,1]),],col="blue",type="l",ylim=ylim,lty=1,xlab = "single index",ylab = ylab,...)
-  if(plot_data){points(UQ[order(UQ[,1]),1],(model_obj$model$y)[order(UQ[,1])],pch=20)}
-
-    # UQ = cbind(model_obj$model$a,result$fitted.values)
-    # UQ0 = UQ[index==0,]
-    # UQ1 = UQ[index==1,]
-    # y_scale = range(result$fitted.values)
-    # plot(UQ0[order(UQ0[,1]),],type="l",ylim=y_scale,lty=2,...)
-    # lines(UQ1[order(UQ1[,1]),],type="l",lty=2,...)
+  #if(plot_data){points(UQ[order(UQ[,1]),1],(model_obj$model$y)[order(UQ[,1])],pch=20)}
 }
 
-#summary function of gplsim object
-summary.gplsim <- function(object){
+#' Summary function of gplsim object
+#' 
+#' @param object the gam/gplism fitted object
+#' @param ... optional arguments
+#'
+#' @return gplsim_obj a list of summary information for a fitted gplsim object, which extends on gam object.
+#' @export
+summary.gplsim <- function(object,...){
   gplsim_obj <- mgcv::summary.gam(object)
   #single index term
   p.table.sim <- matrix(object$theta,ncol=1)
@@ -139,7 +271,15 @@ summary.gplsim <- function(object){
   return(gplsim_obj)
 }
 
-#print summary function
+#' Print Summary function of gplsim object
+#' 
+#' @param x the gam/gplism fitted object
+#' @param digits controls number of digits printed in output.
+#' @param signif.stars should significance stars be printed alongside output.
+#' @param ... optional arguments
+#'
+#' @return summarized object with nice format
+#' @export
 print.summary.gplsim <- function(x, digits = max(5, getOption("digits") - 3),
                               signif.stars = getOption("show.signif.stars"), ...)
   # print method for gam summary method. Improved by Henric Nilsson
@@ -177,8 +317,17 @@ print.summary.gplsim <- function(x, digits = max(5, getOption("digits") - 3),
   invisible(x)
 }
 
-# function dedicated to simulation
-add_sim_bound <- function(data,family = gaussian){
+#' function dedicated to add simulation standard error bound, in development
+#' draw the bound to current plot
+#' 
+#' @param data a list of simulated data
+#' @param family default is gaussian()
+#' @param M number of simulations 
+#' @param n sample size
+#' @param true.theta the true coefficients
+#'
+#' @return NULL
+add_sim_bound <- function(data,family = gaussian(),M=M,n=n,true.theta=true.theta){
   offset_fit_matrix <- matrix(0, M, n)
   for (i in 1:M){
     y=(data$Y)[[i]]       # continous response
@@ -188,69 +337,80 @@ add_sim_bound <- function(data,family = gaussian){
     offset_fit <- family$linkinv(model_obj$linear.predictors-model_obj$gamma%*%t(model_obj$model$z))
     offset_fit_matrix[i,] <-  offset_fit
   }
-  quan2.5=apply(offset_fit_matrix, 2, quantile, prob=0.025)
-  quan97.5=apply(offset_fit_matrix, 2, quantile,prob=0.975)
+  quan2.5=apply(offset_fit_matrix, 2, stats::quantile, prob=0.025)
+  quan97.5=apply(offset_fit_matrix, 2, stats::quantile,prob=0.975)
   fit_mean=apply(offset_fit_matrix, 2, mean)
   U = data$X%*%true.theta
-  lines(U[order(U)],quan2.5[order(U)],type="l",lty=2)
-  lines(U[order(U)],quan97.5[order(U)],type="l",lty=2)
-  lines(U[order(U)],fit_mean[order(U)],type="l",lty=1,col="blue")
+  graphics::lines(U[order(U)],quan2.5[order(U)],type="l",lty=2)
+  graphics::lines(U[order(U)],quan97.5[order(U)],type="l",lty=2)
+  graphics::lines(U[order(U)],fit_mean[order(U)],type="l",lty=1,col="blue")
   #result_table
 }
 
-
-
-# supporting function to make tr smooth
+#' supporting function to make tr smooth
+#'
+#' @param object smooth object for gam class
+#' @param data the new data to predict on
+#' @param knots knots
+#'
+#' @return tr smooth object
 smooth.construct.tr.smooth.spec<-function(object,data,knots)
   ## a truncated power spline constructor method function
   ## object$p.order = null space dimension
-{ require(splines)
+{ requireNamespace("splines")
   m <- object$p.order[1]
-if (is.na(m)) m <- 2 ## default
-if (m<1) stop("silly m supplied")
-if (object$bs.dim<0) object$bs.dim <- 10 ## default
-nk<-object$bs.dim-m-1 ## number of knots
-if (nk<=0) stop("k too small for m")
-x <- data[[object$term]]  ## the data
-x.shift <- mean(x) # shift used to enhance stability
-k <- knots[[object$term]] ## will be NULL if none supplied
-if (is.null(k)) # space knots through data
-{ n<-length(x)
-k<-quantile(x[2:(n-1)],seq(0,1,length=nk+2))[2:(nk+1)]
-}
-if (length(k)!=nk) # right number of knots?
-  stop(paste("there should be ",nk," supplied knots"))
-x <- x - x.shift # basis stabilizing shift
-k <- k - x.shift # knots treated the same!
-X<-matrix(0,length(x),object$bs.dim)
-for (i in 1:(m+1)) X[,i] <- x^(i-1)
-for (i in 1:nk) X[,i+m+1]<-(x-k[i])^m*as.numeric(x>k[i])
-object$X<-X # the finished model matrix
-if (!object$fixed) # create the penalty matrix
-{ object$S[[1]]<-diag(c(rep(0,m+1),rep(1,nk)))
-}
-object$rank<-nk  # penalty rank
-object$null.space.dim <- m+1  # dim. of unpenalized space
-## store "tr" specific stuff ...
-object$knots<-k;object$m<-m;object$x.shift <- x.shift
-object$df<-ncol(object$X)     # maximum DoF (if unconstrained)
-class(object)<-"tr.smooth"  # Give object a class
-object }
+  if (is.na(m)) m <- 2 ## default
+  if (m<1) stop("silly m supplied")
+  if (object$bs.dim<0) object$bs.dim <- 10 ## default
+  nk<-object$bs.dim-m-1 ## number of knots
+  if (nk<=0) stop("k too small for m")
+  x <- data[[object$term]]  ## the data
+  x.shift <- mean(x) # shift used to enhance stability
+  k <- knots[[object$term]] ## will be NULL if none supplied
+  if (is.null(k)) # space knots through data
+  { n<-length(x)
+  k<-quantile(x[2:(n-1)],seq(0,1,length=nk+2))[2:(nk+1)]
+  }
+  if (length(k)!=nk) # right number of knots?
+    stop(paste("there should be ",nk," supplied knots"))
+  x <- x - x.shift # basis stabilizing shift
+  k <- k - x.shift # knots treated the same!
+  X<-matrix(0,length(x),object$bs.dim)
+  for (i in 1:(m+1)) X[,i] <- x^(i-1)
+  for (i in 1:nk) X[,i+m+1]<-(x-k[i])^m*as.numeric(x>k[i])
+  object$X<-X # the finished model matrix
+  if (!object$fixed) # create the penalty matrix
+  { object$S[[1]]<-diag(c(rep(0,m+1),rep(1,nk)))
+  }
+  object$rank<-nk  # penalty rank
+  object$null.space.dim <- m+1  # dim. of unpenalized space
+  ## store "tr" specific stuff ...
+  object$knots<-k;object$m<-m;object$x.shift <- x.shift
+  object$df<-ncol(object$X)     # maximum DoF (if unconstrained)
+  class(object)<-"tr.smooth"  # Give object a class
+  object 
+  }
 
+
+#' prediction method function for the tr smooth class
+#'
+#' @param object smooth object for gam class
+#' @param data the new data to predict on
+#''
+#' @return X the prediction matrix
 Predict.matrix.tr.smooth<-function(object,data)
   ## prediction method function for the tr smooth class
-{ require(splines)
+{ requireNamespace("splines")
   x <- data[[object$term]]
-x <- x - object$x.shift # stabilizing shift
-# spline order (3=cubic)
-# knot locations
-# number of knots
-m <- object$m;
-k<-object$knots
-nk<-length(k)
-X<-matrix(0,length(x),object$bs.dim)
-for (i in 1:(m+1)) X[,i] <- x^(i-1)
-for (i in 1:nk) X[,i+m+1] <- (x-k[i])^m*as.numeric(x>k[i])
-X # return the prediction matrix
+  x <- x - object$x.shift # stabilizing shift
+  # spline order (3=cubic)
+  # knot locations
+  # number of knots
+  m <- object$m;
+  k<-object$knots
+  nk<-length(k)
+  X<-matrix(0,length(x),object$bs.dim)
+  for (i in 1:(m+1)) X[,i] <- x^(i-1)
+  for (i in 1:nk) X[,i+m+1] <- (x-k[i])^m*as.numeric(x>k[i])
+  X # return the prediction matrix
 }
-##### end of supporting functions;
--- R/plsim_it.r
+++ R/plsim_it.r
@@ -1,176 +1,6 @@
+#internal support function
 #function to fit plsim with iterative procedure, through non-linear least square
 #input x,y,degree,nknots,alpha0(if different from default OLSe),maxiter,z
-
-
-# normalize alpha0
-
-# reduce X dimensionality by this projection.
-
-# get predicted yhat
-
-
-
-#
-
-
-
-# nonlinear optimization
-
-
-
-#plresim is the residual vector of length n
-plresim <- function(param,x,y,z,degree,nknots,lambda){
-
-  m <- length(param)
-  d <- NCOL(x)
-  alpha <- param[1:d]
-  alpha <- sign(alpha[1])*alpha/sqrt(sum(alpha^2))
-  beta <- param[(d+1):length(param)]
-
-
-
-  xnew <- x%*%alpha # reduce x dimentionality by this projection.
-  knots <- quantileknots(xnew,nknots)
-  xm <- powerbasis(xnew,degree,knots)
-  exty <- c(y,  matrix(0,nrow=nknots,ncol=1))
-
-  if(is.null(z)){
-    yhat <- xm%*%beta
-  }else{
-    yhat <- cbind(xm,z)%*%beta
-  }
-  g <- exty - c(yhat, sqrt(lambda)*beta[(degree+2):(degree+nknots+1)])
-  return(g)
-}
-
-quantileknots <- function(x,nknots){
-  boundstab <- 0
-  n <- length(x)
-  xsort <- sort(x)
-  loc <- t(n*(1:nknots+2*boundstab)) / (nknots+1+2*boundstab)
-  knots <- xsort[round(loc)]
-  knots <- knots[(1 + boundstab) : (nknots + boundstab)]
-  return(knots)
-}
-
-powerbasis <- function(x,degree,knots,der=0){
-  n <- NROW(x)
-  nknots <- length(knots)
-
-  if (der == 0 ){
-    xm <- rep(1,n)
-  }else{
-    xm <- rep(0,n)
-  }
-
-  for (i in 1:degree ){
-    if (i < der ){
-      xm <- cbind(xm, rep(0,n))
-    }else{
-      xm <- cbind(xm, prod((i-der+1):i) * x^(i-der))
-    }
-  }
-
-  if (nknots > 0 ){
-    for (i in 1:(nknots) ){
-      xm <- cbind(xm,prod((degree-der+1):degree) * (x-knots[i])^(degree-der)*(x > knots[i]))
-    }
-  }
-  xm
-}
-
-
-pbderiv1 <- function(x,degree,knots){
-  n <- NROW(x)
-  nknots <- length(knots)
-
-  xm1 <- rep(0,n)
-
-  for (i in 1:degree ){
-    xm1 <- cbind(xm1,i*x^(i-1))
-  }
-
-  if (nknots > 0 ){
-    for (i in 1:(nknots) ){
-      xm1 <- cbind(xm1, degree*((x-knots[i])^(degree-1))*(x > knots[i]))
-    }
-  }
-  return(xm1)
-}
-
-srspl <- function(x,y,degree=3,nknots=10,z=NULL,penwt=(10^seq(-6, 7, length.out=n))){
-  n = NROW(x)
-  #ngrid = 200
-  #ugrid = t(seq(0.4,1.4,length.out=ngrid))
-  #knots = quantileknots(ugrid,nknots);
-  knots = quantileknots(x,nknots)
-  n = length(x) ;
-  xm=rep(1,n);
-  xm = powerbasis(x,degree,knots) ;
-  xm = cbind(xm,z);
-  xm1= pbderiv1(x,degree,knots);
-
-
-  xx = t(xm)%*%xm ;
-  xy = t(xm)%*%y ;
-  nz = (!is.null(z))*NCOL(z)
-  id = diag(c(rep(0,(degree+1)),rep(1,(nknots)),rep(0,(nz)))) ;
-  m = length(penwt) ;
-  beta = matrix(0,NCOL(xm),m);
-  yhat = matrix(0,n,m) ;
-  asr = matrix(0,m,1) ;
-  gcv = asr ;
-  trsd = asr ;
-  ssy = t(y)%*%y ;
-  for (i in 1:m ){
-    #return(xx+ penwt[i]*id)
-    b <- solve(xx + penwt[i]*id,tol = 0)
-    xxb <- xx%*%b
-    trsd[i] <- sum(diag(xxb))
-    beta[,i] <- b %*% xy
-    asr[i] <-  (ssy - 2*t(xy)%*%beta[,i] + t(beta[,i])%*%xx%*%beta[,i])/n
-    if (i==1){
-      trsdsd <- sum(diag(xxb*xxb))
-      sigma2hat <-  n*asr[i]/(n-2*trsd[i]+trsdsd)
-    }
-    gcv[i] <- asr[i] / ((1-trsd[i]/n)^2 )
-  }
-
-  imin <- which(  (gcv==min(gcv)) )
-  df <-  trsd
-  dfs <- trsd[imin]
-  sigma2hat <- n*asr[imin] / (n - df[imin])
-  b <- solve(xx + penwt[imin]*id,tol = 0)
-  beta <- beta[,imin]
-  yhat <- xm%*%beta
-  yhat1 <- xm1%*%beta[1:(1+degree+nknots)] # estimate of the first derivative of y
-  postvarbeta <- sigma2hat*b
-  postvaryhat <- sigma2hat*(xm*(xm%*%b))%*%matrix(1,length(beta),1)
-  lambda <- penwt[imin]
-  res <- NULL
-  res$yhat = yhat
-  res$beta = beta
-  res$yhat1 = yhat1
-  res$gcv =gcv
-  res$imin = res$imin
-  res$lambda = lambda
-  return(res)
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 plsiml <- function(y,x,z=NULL,degree=3,nknots=12,alpha0=NULL,maxiter=2){
 
   if(is.null(z)==TRUE){
@@ -244,7 +74,6 @@ plsiml <- function(y,x,z=NULL,degree=3,nknots=12,alpha0=NULL,maxiter=2){
   lambda = srspl_fit$lambda
 
   mse <- 1/n*sum((y-yhat)^2)
-
   res <- NULL
   res$param <- param
   res$mse <- mse
@@ -252,6 +81,160 @@ plsiml <- function(y,x,z=NULL,degree=3,nknots=12,alpha0=NULL,maxiter=2){
   res$iter <- iter
   res$alpha <- alpha
   return(res)
-
 }
+
+##internal support function
+#plresim is the residual vector of length n
+plresim <- function(param,x,y,z,degree,nknots,lambda){
+
+  m <- length(param)
+  d <- NCOL(x)
+  alpha <- param[1:d]
+  alpha <- sign(alpha[1])*alpha/sqrt(sum(alpha^2))
+  beta <- param[(d+1):length(param)]
+
+
+
+  xnew <- x%*%alpha # reduce x dimentionality by this projection.
+  knots <- quantileknots(xnew,nknots)
+  xm <- powerbasis(xnew,degree,knots)
+  exty <- c(y,  matrix(0,nrow=nknots,ncol=1))
+
+  if(is.null(z)){
+    yhat <- xm%*%beta
+  }else{
+    yhat <- cbind(xm,z)%*%beta
+  }
+  g <- exty - c(yhat, sqrt(lambda)*beta[(degree+2):(degree+nknots+1)])
+  return(g)
+}
+
+#internal support function
+quantileknots <- function(x,nknots){
+  boundstab <- 0
+  n <- length(x)
+  xsort <- sort(x)
+  loc <- t(n*(1:nknots+2*boundstab)) / (nknots+1+2*boundstab)
+  knots <- xsort[round(loc)]
+  knots <- knots[(1 + boundstab) : (nknots + boundstab)]
+  return(knots)
+}
+
+#internal support function
+powerbasis <- function(x,degree,knots,der=0){
+  n <- NROW(x)
+  nknots <- length(knots)
+
+  if (der == 0 ){
+    xm <- rep(1,n)
+  }else{
+    xm <- rep(0,n)
+  }
+
+  for (i in 1:degree ){
+    if (i < der ){
+      xm <- cbind(xm, rep(0,n))
+    }else{
+      xm <- cbind(xm, prod((i-der+1):i) * x^(i-der))
+    }
+  }
+
+  if (nknots > 0 ){
+    for (i in 1:(nknots) ){
+      xm <- cbind(xm,prod((degree-der+1):degree) * (x-knots[i])^(degree-der)*(x > knots[i]))
+    }
+  }
+  xm
+}
+
+#internal support function
+pbderiv1 <- function(x,degree,knots){
+  n <- NROW(x)
+  nknots <- length(knots)
+
+  xm1 <- rep(0,n)
+
+  for (i in 1:degree ){
+    xm1 <- cbind(xm1,i*x^(i-1))
+  }
+
+  if (nknots > 0 ){
+    for (i in 1:(nknots) ){
+      xm1 <- cbind(xm1, degree*((x-knots[i])^(degree-1))*(x > knots[i]))
+    }
+  }
+  return(xm1)
+}
+
+#internal support function
+srspl <- function(x,y,degree=3,nknots=10,z=NULL,penwt=(10^seq(-6, 7, length.out=n))){
+  n = NROW(x)
+  #ngrid = 200
+  #ugrid = t(seq(0.4,1.4,length.out=ngrid))
+  #knots = quantileknots(ugrid,nknots);
+  knots = quantileknots(x,nknots)
+  n = length(x) ;
+  xm=rep(1,n);
+  xm = powerbasis(x,degree,knots) ;
+  xm = cbind(xm,z);
+  xm1= pbderiv1(x,degree,knots);
+
+
+  xx = t(xm)%*%xm ;
+  xy = t(xm)%*%y ;
+  nz = (!is.null(z))*NCOL(z)
+  id = diag(c(rep(0,(degree+1)),rep(1,(nknots)),rep(0,(nz)))) ;
+  m = length(penwt) ;
+  beta = matrix(0,NCOL(xm),m);
+  yhat = matrix(0,n,m) ;
+  asr = matrix(0,m,1) ;
+  gcv = asr ;
+  trsd = asr ;
+  ssy = t(y)%*%y ;
+  for (i in 1:m ){
+    #return(xx+ penwt[i]*id)
+    b <- solve(xx + penwt[i]*id,tol = 0)
+    xxb <- xx%*%b
+    trsd[i] <- sum(diag(xxb))
+    beta[,i] <- b %*% xy
+    asr[i] <-  (ssy - 2*t(xy)%*%beta[,i] + t(beta[,i])%*%xx%*%beta[,i])/n
+    if (i==1){
+      trsdsd <- sum(diag(xxb*xxb))
+      sigma2hat <-  n*asr[i]/(n-2*trsd[i]+trsdsd)
+    }
+    gcv[i] <- asr[i] / ((1-trsd[i]/n)^2 )
+  }
+
+  imin <- which(  (gcv==min(gcv)) )
+  df <-  trsd
+  dfs <- trsd[imin]
+  sigma2hat <- n*asr[imin] / (n - df[imin])
+  b <- solve(xx + penwt[imin]*id,tol = 0)
+  beta <- beta[,imin]
+  yhat <- xm%*%beta
+  yhat1 <- xm1%*%beta[1:(1+degree+nknots)] # estimate of the first derivative of y
+  postvarbeta <- sigma2hat*b
+  postvaryhat <- sigma2hat*(xm*(xm%*%b))%*%matrix(1,length(beta),1)
+  lambda <- penwt[imin]
+  res <- NULL
+  res$yhat = yhat
+  res$beta = beta
+  res$yhat1 = yhat1
+  res$gcv =gcv
+  res$imin = res$imin
+  res$lambda = lambda
+  return(res)
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
